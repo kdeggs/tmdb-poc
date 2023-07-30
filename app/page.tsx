667b3dde9225f3910a469c25029f3ab4036dc262
Kyler Deggs <deggskyler@gmail.com>
2023-07-30 02:33:27 -0500

    Updated landing page

--- app/page.tsx
+++ app/page.tsx
@@ -39,6 +39,12 @@ export default function Home() {
         />
       </div>
 
+      <div>
+        <p>
+          Hello from Kyler!
+        </p>
+      </div>
+
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
